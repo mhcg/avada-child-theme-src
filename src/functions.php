154cb7116ac23e76a0a395a1818dfb444a17cf46
Mark Heydon <markheydon@users.noreply.github.com>
2024-02-23 01:44:16 +0000

    Include placeholders for using Google Tag Manager Fixes #2

--- src/functions.php
+++ src/functions.php
@@ -26,3 +26,12 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+/**
+ * Add Google Tag Manager code and data layer.
+ */
+require_once get_theme_file_path( 'inc/gtm-helpers.php' );
+
+add_action( 'wp_head', 'mhcg_add_gtm_data_layer' );
+add_action( 'wp_head', 'mhcg_add_gtm_head_code' );
+add_action( 'avada_before_body_content', 'mhcg_add_gtm_to_body' );
